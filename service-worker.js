73f9eca694a10b2ac5c11d05be24be5b56c0e1c0
sakibsarkar <sakibsarkar707@gmail.com>
2023-09-23 20:36:09 +0600

    offline functionality added

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,39 @@
-// // Inside service-worker.js
-// self.addEventListener('install', function (event) {
-//     event.waitUntil(
-//         caches.open('my-cache-name').then(function (cache) {
-//             return cache.addAll([
-//                 './',
-//                 './index.html',
-//                 './style.css',
-//                 './script.js',
-//                 './media.css'
+// Inside service-worker.js
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('my-cache-name').then(function (cache) {
+            return cache.addAll([
+                './',
+                './index.html',
+                './style.css',
+                './script.js',
+                './media.css'
 
 
-//                 // Add more resources to cache as needed
-//             ]);
-//         })
-//     );
-// });
+                // Add more resources to cache as needed
+            ]);
+        })
+    );
+});
 
-// self.addEventListener('fetch', function (event) {
-//     event.respondWith(
-//         caches.match(event.request).then(function (response) {
-//             return response || fetch(event.request);
-//         })
-//     );
-// });
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
 
-// self.addEventListener('activate', function (event) {
-//     event.waitUntil(
-//         caches.keys().then(function (cacheNames) {
-//             return Promise.all(
-//                 cacheNames.map(function (cacheName) {
-//                     if (cacheName !== 'my-cache-name') {
-//                         return caches.delete(cacheName);
-//                     }
-//                 })
-//             );
-//         })
-//     );
-// });
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== 'my-cache-name') {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
